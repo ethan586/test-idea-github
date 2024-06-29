64ec72dc1fd761ced0d12175f1159c474f4e0e2a
ethan586 <ethan7396@qq.com>
2024-06-29 09:46:32 +0800

    Update test.java
    main分支添加新功能：打印“这个测试是来自github的main分支”

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Data!");
+        System.out.println("This test comes from the main branch of GitHub");
     }
 }
