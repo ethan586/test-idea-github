cef018812f0d51e61f2ecc120ebc51e4f76acccb
ethan586 <ethan7396@qq.com>
2024-06-29 09:38:34 +0800

    master分支新增新功能：打印“这个测试来自master”

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello Data!");
+        System.out.println("Hello test!");
+        System.out.println("this test comes from master");
     }
 }
